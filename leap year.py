b23480f365c4ee7c6d6b95cad9df272e9ba399cb
Raseent <110896636+Raseent@users.noreply.github.com>
2022-08-24 14:44:49 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,14 @@
+def leap_year(y):
+    if y % 400 == 0:
+        return True
+    if y % 100 == 0:
+        return False
+    if y % 4 ==0:
+        return True
+    else:
+        return False
+print(leap_year(1990))
+print(leap_year(2004))
+print(leap_year(2022))
+print(leap_year(2008))
+
