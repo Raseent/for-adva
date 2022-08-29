f3ffb36a6069e3e41ee1875658d65f1d5098fb03
Raseent <110896636+Raseent@users.noreply.github.com>
2022-08-29 14:21:44 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,8 @@
+for i in range(0, 10):
+    for j in range(0, i + 1):
+        print("*", end="")
+    print("\r")
+for i in range(5):
+    for j in range(i):
+        print(i,end="")
+    print("\n")
