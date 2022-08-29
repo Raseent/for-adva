d2bee4170d1808498c15132f56d1f2018d5c8979
Raseent <110896636+Raseent@users.noreply.github.com>
2022-08-29 14:48:35 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -6,3 +6,16 @@ for i in range(5):
     for j in range(i):
         print(i,end="")
     print("\n")
+    
+
+b=0
+for i in range(5,0,-1):
+    b+=1
+    for j in range(1,i+1):
+        print(b,end="")
+    print("\r")
+
+for i in range(5,0,-1):
+    for j in range(0,i):
+         print(10,end="")
+    print("\r")
